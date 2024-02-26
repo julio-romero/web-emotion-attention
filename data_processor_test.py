dabcc018efeaf1a6ae46e1fccdcdb1d39a4bc12d
julio-romero <77467500+julio-romero@users.noreply.github.com>
2024-02-26 09:28:46 +0000

    Squashed commit of the following:
    commit d0a57ddd6da137ef7666194d562255d6c1e7ee75
    Merge: b15c761 7ae97d2
    Author: julio-romero <77467500+julio-romero@users.noreply.github.com>
    Date:   Mon Feb 26 09:26:23 2024 +0000
    
        Merge branch 'dev-manuel' of https://github.com/julio-romero/web-emotion-attention into dev-manuel
    
    commit b15c76170e6c2582c6fd6c151d510ff056c2c5c3
    Author: julio-romero <77467500+julio-romero@users.noreply.github.com>
    Date:   Mon Feb 26 09:25:50 2024 +0000
    
        Refactor tests
    
    commit 7ae97d2a2313a2acf6e71c064ab4b2ddba2abb26
    Author: davegmn <122129821+davegmn@users.noreply.github.com>
    Date:   Sun Feb 25 23:59:48 2024 +0000
    
        Update core.py
    
    commit d26dab20ed5f51cac240fa2d5b14c2ce8279da1d
    Author: julio-romero <77467500+julio-romero@users.noreply.github.com>
    Date:   Fri Feb 23 15:15:45 2024 +0000
    
        typo
    
    commit 4fda2eb3dbcd54ef9b0e41f6ce31535c01cfbfaf
    Author: julio-romero <77467500+julio-romero@users.noreply.github.com>
    Date:   Fri Feb 23 15:02:04 2024 +0000
    
        It works now :diamond_shape_with_a_dot_inside:
    
    commit 8ce2fae22df194f36d77fa270609ab7c150ebd04
    Author: julio-romero <77467500+julio-romero@users.noreply.github.com>
    Date:   Fri Feb 23 14:33:58 2024 +0000
    
        Chrome driver not working
    
    commit f945156e151bd40193774bd96f23deb44de0d32b
    Author: julio-romero <77467500+julio-romero@users.noreply.github.com>
    Date:   Fri Feb 23 13:46:39 2024 +0000
    
        Squashed commit of the following:
    
        commit 6d898271465b2ca382a6c4944ad7dde31a048949
        Author: leslygv <95459594+aa01753039@users.noreply.github.com>
        Date:   Fri Feb 23 13:44:25 2024 +0000
    
            Dev lesly1 (#2)
    
            * Added web class and data processing functions
    
            * Refactor function to improve performance and readability
    
            * Add heatmap plotting functionality
    
            ---------
    
            Co-authored-by: Lesly C Velez <leslygv12@gmail.com>
    
    commit 9fe5a5f019517e5d174b47a948e4339835b3b200
    Author: julio-romero <77467500+julio-romero@users.noreply.github.com>
    Date:   Fri Feb 23 13:45:12 2024 +0000
    
        Clean up
    
    commit 3af3bb07543eaccbfd5b615349af7c81d2003734
    Author: julio-romero <77467500+julio-romero@users.noreply.github.com>
    Date:   Fri Feb 23 13:16:41 2024 +0000
    
        Squashed commit of the following:
    
        commit 57a35291665718c52ccd50d4e25665edb304ab61
        Author: Lesly C Velez <leslygv12@gmail.com>
        Date:   Fri Feb 23 13:15:13 2024 +0000
    
            Refactor function to improve performance and readability
    
        commit 63ca5a12ef69c44464a60e77a8a432ff22515433
        Author: Lesly C Velez <leslygv12@gmail.com>
        Date:   Thu Feb 22 20:57:50 2024 +0000
    
            Added web class and data processing functions

--- data_processor_test.py
+++ data_processor_test.py
@@ -1,12 +1,15 @@
 from multimotions.core import DataProcessor
 
+def test_data_processor_creation():
+    processor = DataProcessor("data/scroll_data.csv", "data/imotions_data.csv", "data/")
+    assert processor is not None
 
-def test_data_processor():
-    processor = DataProcessor("target", None, "target")
-    try:
-        processor.start_monitoring(5)
-    except KeyboardInterrupt:
-        processor.plot_data()
+def test_data_processing():
+    processor = DataProcessor("data/scroll_data.csv", "data/imotions_data.csv", "data/")
+    processor.process_data()
+    assert processor.merged_data is not None
+
 
 if __name__ == "__main__":
-    test_data_processor()
+    test_data_processor_creation()
+    test_data_processing()
